4af40728bc2270033bc9400c89bb4e50fc86f967
xFijo <75216577+xFijo@users.noreply.github.com>
2021-07-18 18:44:35 +0100

    Create bot.js

---
+++ bot.js
@@ -0,0 +1,11 @@
+   if (command === "rps") {
+        const options = [
+            "rock :shell: ",
+            "paper :newspaper2:",
+            "scissors :scissors: "
+        ]
+        const option = options[Math.floor(Math.random() * options.length)]
+        message.channel.send(`You got ${option}`);
+    }
+
+});
