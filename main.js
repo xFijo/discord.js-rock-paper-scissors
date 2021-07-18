60091d83f14499f4da66fece0d0d4dbf3258a06e
xFijo <75216577+xFijo@users.noreply.github.com>
2021-07-18 18:45:18 +0100

    Create main.js

---
+++ main.js
@@ -0,0 +1,10 @@
+// for in command handler
+
+const options = [
+        "rock :shell: ",
+        "paper :newspaper2:",
+         "scissors :scissors: "
+     ]
+     const option = options[Math.floor(Math.random() * options.length)]
+     message.channel.send(`You got ${option}`);
+    };
